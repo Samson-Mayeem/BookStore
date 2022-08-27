a230ece2051d4420cb266a8b5ad1c84dea8c52d5
Samson-Mayeem <63527034+Samson-Mayeem@users.noreply.github.com>
2022-08-27 02:43:06 +0000

    auth been modified

--- DataAccessLib/Models/Userss.cs
+++
@@ -1,9 +0,0 @@
-namespace BookStore.Models
-{
-    public class Userss
-    {
-        public int Id { get; set; }
-        public string Email { get; set; }
-        public int Password { get; set; }
-    }
-}
